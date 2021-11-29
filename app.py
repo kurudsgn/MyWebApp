a2612ee87b9cc45ec8ef36d2295b5cbba7eedfdf
kurudsgn <58908287+kurudsgn@users.noreply.github.com>
2021-11-29 19:08:37 +0300

    v2

--- app.py
+++ app.py
@@ -5,8 +5,25 @@ import psycopg2
 app = Flask(__name__)
 
 
+conn = psycopg2.connect(database="service_db",
+                        user="<логин создателя БД>",
+                        password="",
+                        host="localhost",
+                        port="5432")
+
+cursor = conn.cursor()
+
+
 @app.route('/login/', methods=['GET'])
 def index():
     return render_template('login.html')
 
 
+@app.route('/login/', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+    records = list(cursor.fetchall())
+
+    return render_template('account.html', full_name=records[0][1])
